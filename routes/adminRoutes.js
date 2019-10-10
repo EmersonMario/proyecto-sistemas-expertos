844fad6e33ec59b66816950a2129ab2b88c9a298
Émerson Mario Izaguirre Portillo <31661777+EmersonMario@users.noreply.github.com>
2019-10-09 19:19:05 -0600

    Add files via upload

--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var adminController = require('../controllers/adminController');
+var {isUserAuthenticated} = require('../config/customFunctions');
 
-
-router.all('/*', (peticion, respuesta, siguiente)=>{
+router.all('/*', isUserAuthenticated, (peticion, respuesta, siguiente)=>{
     peticion.app.locals.layout = 'admin';
     siguiente();
 });
@@ -12,6 +12,49 @@ router.route('/').get(adminController.index);
 
 router.route('/posts').get(adminController.getPosts);
 
-router.route('/posts/create').get(adminController.createPosts).post(adminController.submitPosts);
+router.route('/posts/create').get(adminController.createPostsGet).post(adminController.submitPosts);
+
+router.route('/posts/edit/:id').get(adminController.editPost).put(adminController.editPostSubmit);
+
+router.route('/posts/delete/:id').delete(adminController.deletePost);
+
+/*Rutas del administrador de categorías*/
+
+router.route('/category').get(adminController.getCategories).post(adminController.createCategories);
+
+router.route('/category/create').post(adminController.createCategories);
+
+router.route('/category/edit/:id').get(adminController.editCategoriesGetRoute).post(adminController.editCategoriesPostRoute);
+
+router.route('/category/delete/:id').delete(adminController.deleteCategory);
+
+/*Rutas del administrador de comentarios */
+router.route('/comment').get(adminController.getComments);
+
+router.route('/comment/edit/:id').put(adminController.approveComment);
+
+/*Rutas del administrador de usuarios*/
+router.route('/user').get(adminController.getUsers);
+
+router.route('/user/edit/:id').put(adminController.adminTrue);
+
+/*Rutas del administrador de configuraciones*/
+router.route('/configuration').get(adminController.getConfigurations);
+
+router.route('/configuration/editFavicon').put(adminController.editFileFavicon);
+
+router.route('/configuration/editLogo').put(adminController.editFileLogo);
+
+router.route('/configuration/editPageTitle').put(adminController.editPageTitle);
+
+router.route('/configuration/editPageDescription').put(adminController.editPageDescription);
+
+router.route('/configuration/editPageKeyWords').put(adminController.editPageKeyWords);
+
+router.route('/template').get(adminController.getTemplates);
+
+router.route('/template/create').get(adminController.createTemplate).post(adminController.submitTemplates);
+
+router.route('/template/edit/:id').put(adminController.activeTemplate);
 
 module.exports = router;
--- routes/defaultRoutes.js
+++ routes/defaultRoutes.js
@@ -1,7 +1,11 @@
 var express = require("express");
 var router = express.Router();
 var defaultController = require('../controllers/defaultController');
-
+var passport = require('passport');
+var localStrategy = require('passport-local').Strategy;
+var bcrypt = require('bcryptjs');
+var User = require('../models/UserModel').User;
+var Template = require('../models/TemplateModel').Template;
 
 /*Configuración de las rutas*/
 router.all('/*', (peticion, respuesta, siguiente)=>{
@@ -9,10 +13,56 @@ router.all('/*', (peticion, respuesta, siguiente)=>{
     siguiente();
 });
 
-router.route('/').get( defaultController.index);
+router.route('/').get(defaultController.index);
 
-router.route('/login').get(defaultController.loginGet).post(defaultController.loginPost);
+//Definiendo 
+passport.use(new localStrategy({
+    usernameField: 'email',
+    passReqToCallback: true
+}, (peticion, email, password, done)=>{
+    User.findOne({email: email}).then(user=>{
+        if(!user){
+            return done(null, false, peticion.flash('error-message', 'Usuario no encontrado con este correo electrónico'));
+        }
+        bcrypt.compare(password, user.password, (err, passwordMatched)=>{
+            if(err){
+                return err;
+            }
+            if(!passwordMatched){
+                return done(null, false, peticion.flash('error-message', 'Usuario inválido o contraseña incorrecta'));
+            }
+            return done(null, user, peticion.flash('success-message', 'Ha iniciado sesión satisfactoriamente'))
+        });
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+  
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+router.route('/login').get(defaultController.loginGet).post(passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    failureFlash: true,
+    successFlash: true,
+    session: true
+}), defaultController.loginPost);
 
 router.route('/register').get(defaultController.registerGet).post(defaultController.registerPost);
+
+router.route('/post/:id').get(defaultController.getSinglePost).post(defaultController.submitComment);
+
+router.get('/logout', (peticion, respuesta)=>{
+    peticion.logOut();
+    peticion.flash('success-message', 'Se ha cerrado sesión satisfactoriamente');
+    respuesta.redirect('/');
+});
+
 
 module.exports = router;
