bf77f73229d344fd202437e644dc02a7a4ad9de3
Émerson Mario Izaguirre Portillo <31661777+EmersonMario@users.noreply.github.com>
2019-10-09 19:11:09 -0600

    Add files via upload

--- config/configuration.js
+++ config/configuration.js
@@ -2,9 +2,10 @@ module.exports = {
     mongoDbUrl: 'mongodb://localhost:27017/tylox',
     PORT: process.env.PORT || 3000,
     globalVariables: (peticion, respuesta, siguiente)=>{
-        respuesta.locals.success_message = peticion.flash('success message');
-        respuesta.locals.error_message = peticion.flash('error message');
-    
+        respuesta.locals.success_message = peticion.flash('success-message');
+        respuesta.locals.error_message = peticion.flash('error-message');
+        respuesta.locals.user = peticion.user || null;
+        respuesta.locals.configuration = peticion.configuration || null;
         siguiente();
     }
 };
