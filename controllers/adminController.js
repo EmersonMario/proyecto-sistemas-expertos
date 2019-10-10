080db72a458ae5461c5b3fac39fd7dd6530f1d5a
Émerson Mario Izaguirre Portillo <31661777+EmersonMario@users.noreply.github.com>
2019-10-09 19:11:29 -0600

    Add files via upload

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,27 +1,353 @@
+var fs = require('fs');
 var Post = require('../models/postModel').Post;
+var Template = require('../models/TemplateModel').Template;
+var Category = require('../models/CategoryModel').Category;
+var Comment = require('../models/CommentModel').Comment;
+var User = require('../models/UserModel').User;
+var Configuration = require('../models/ConfigurationModel').Configuration;
+var {isEmpty} = require('../config/customFunctions');
+
 
 module.exports = {
     index: (peticion, respuesta)=>{
         respuesta.render('admin/index');
     },
     getPosts: (peticion, respuesta)=>{
-        respuesta.render('admin/posts/index');
+        Post.find().populate('category').then(posts =>{
+            respuesta.render('admin/posts/index', {posts: posts});
+        });
+        
+    },
+    createPostsGet: (peticion, respuesta) => {
+        Category.find().then(cats => {
+            respuesta.render('admin/posts/create', {categories: cats});
+        });
     },
     submitPosts: (peticion, respuesta)=>{
+        const commentsAllowed = peticion.body.allowComments ? true:false;
+    
+        //Validación del Input File
+        let fileName = '';
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.uploadedFile;
+            fileName = file.name;
+            let uploadDir = './public/uploads/';
+
+            file.mv(uploadDir+fileName, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
         const newPost = new Post({
             title: peticion.body.title,
-            autor: peticion.body.autor,
-            description: peticion.body.description,
-            category: peticion.body.category
+            content: peticion.body.content,
+            user: peticion.body.user,
+            file: `uploads/${fileName}`,
+            category: peticion.body.category,
+            allowComments: commentsAllowed
         });
 
         newPost.save().then(post =>{
             console.log(post);
-            peticion.flash('success message', 'Post creado satisfactoriamente.');
+            peticion.flash('success-message', 'Post creado satisfactoriamente.');
             respuesta.redirect('/admin/posts');
         });
     },
-    createPosts: (peticion, respuesta)=>{
-        respuesta.render('admin/posts/create');
+    editPost: (peticion, respuesta)=>{
+        const id = peticion.params.id;
+
+        Post.findById(id).then(post=>{
+            Category.find().then(cats => {
+                respuesta.render('admin/posts/edit', {post: post, categories: cats});
+            });
+        });  
     },
-};
+    editPostSubmit: (peticion, respuesta)=>{
+        const commentsAllowed = peticion.body.allowComments ? true:false;
+
+        const id = peticion.params.id;
+
+        var fileNameEdit = '';
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.uploadedFileEdit;
+            fileNameEdit = file.name;
+            let uploadDir = './public/uploads/';
+
+            file.mv(uploadDir+fileNameEdit, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        Post.findById(id).then(post=>{
+            post.title = peticion.body.title;
+            post.category = peticion.body.category;
+            post.allowComments = peticion.body.allowComments;
+            post.content = peticion.body.content;
+            post.file = `uploads/${peticion.body.uploadedFileEdit}`;
+
+            post.save().then(updatePost=>{
+                peticion.flash('success-message', `El post ${updatePost.title} ha sido actualizado`);
+                respuesta.redirect('/admin/posts');
+            });
+        });
+
+    },
+    deletePost: (peticion, respuesta)=>{
+        Post.findByIdAndDelete(peticion.params.id).then(deletedPost=>{
+            peticion.flash('success-message', `El post ${deletedPost.title} ha sido eliminado.`);
+            respuesta.redirect('/admin/posts');
+        });
+    },
+    /*Todas los metodos para las categorías*/
+    getCategories: (peticion, respuesta)=>{
+        Category.find().then(cats=>{
+            respuesta.render('admin/category/index', {categories: cats});
+        });
+    },
+    createCategories: (peticion, respuesta)=>{
+        var categoryName = peticion.body.name;
+        if(categoryName){
+            const newCategory = new Category({
+                title: categoryName
+            });
+            newCategory.save().then(category => {
+                respuesta.status(200).json(category);
+            })
+        }
+    },
+    editCategoriesGetRoute: async (peticion, respuesta)=>{
+        const catId = peticion.params.id;
+        const cats = await Category.find();
+
+        Category.findById(catId).then(cat =>{
+            respuesta.render('admin/category/edit', {category: cat, categories: cats});
+        })
+
+    },
+    editCategoriesPostRoute: (peticion, respuesta)=>{
+        const catId = peticion.params.id;
+        const newTitle = peticion.body.name;
+        
+        if(newTitle){
+            Category.findById(catId).then(category=>{
+                category.title= newTitle;
+                category.save().then(updated=>{
+                    respuesta.status(200).json({url: '/admin/category'});
+                });
+            });
+        }
+    },
+    deleteCategory: (peticion, respuesta)=>{
+        Category.findByIdAndDelete(peticion.params.id).then(deletedCategory=>{
+            peticion.flash('success-message', `El post ${deletedCategory.title} ha sido eliminado.`);
+            respuesta.redirect('/admin/category');
+        });
+    },
+    /*Todos los metodos para los comentarios*/
+    getComments: (peticion, respuesta)=>{
+        Comment.find().populate('user').then(comments=>{
+            respuesta.render('admin/comments/index', {comments: comments});
+        });
+    },
+    approveComment: (peticion, respuesta)=>{
+        const id = peticion.params.id;
+
+        Comment.findById(id).then(comment=>{
+            comment.commentIsApproved = true;
+
+            comment.save().then(updateComment=>{
+                peticion.flash('success-message', `El comentario ha sido aprobado`);
+                respuesta.redirect('/admin/comment');
+            });
+        });
+    },
+    /*Todos los metodos para los usuarios*/
+    getUsers: (peticion, respuesta)=>{
+        User.find().populate('comment').then(users=>{
+            respuesta.render('admin/users/index', {users: users});
+        });
+    },
+    adminTrue: (peticion, respuesta)=>{
+        const id = peticion.params.id;
+
+        User.findById(id).then(user=>{
+            user.isAdmin = true;
+
+            user.save().then(updateUser=>{
+                peticion.flash('success-message', `Usuario ya es administrador`);
+                respuesta.redirect('/admin/user');
+            });
+        });
+    },
+    /*Todos los metodos para las configuraciones*/
+    getConfigurations: (peticion, respuesta)=>{
+        Configuration.find().then(configurations =>{
+            respuesta.render('admin/configurations/index', {configurations: configurations});
+        });
+    },
+    editFileFavicon: (peticion, respuesta)=>{
+        let fileName = 'favicon.ico';
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.fileFavicon;
+            //fileName = file.name;
+            let uploadDir = './public/uploads/';
+
+            file.mv(uploadDir+fileName, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        Configuration.findById('5d8561039709f1199c921feb').then(editFile1=>{
+            editFile1.fileFavicon = `uploads/${fileName}`;
+
+            editFile1.save().then(updatedFile1=>{
+                peticion.flash('success-message', `Favicon ha sido actualizado satisfactoriamente`);
+                respuesta.redirect('/admin/configuration');
+            });
+        });
+    },
+    editFileLogo: (peticion, respuesta)=>{
+        let fileName = 'logo.png';
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.fileLogo;
+            //fileName = file.name;
+            let uploadDir = './public/uploads/';
+
+            file.mv(uploadDir+fileName, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        Configuration.findById('5d8561039709f1199c921feb').then(editFile2=>{
+            editFile2.fileLogo = `uploads/${fileName}`;
+
+            editFile2.save().then(updatedFile2=>{
+                peticion.flash('success-message', `Logotipo ha sido actualizado satisfactoriamente`);
+                respuesta.redirect('/admin/configuration');
+            });
+        });
+    },
+    editPageTitle: (peticion, respuesta)=>{
+        Configuration.findById('5d8561039709f1199c921feb').then(editTitle=>{
+            editTitle.pageTitle = peticion.body.pageTitle;
+
+            editTitle.save().then(updatedTitle=>{
+                peticion.flash('success-message', `Título ha sido actualizado satisfactoriamente`);
+                respuesta.redirect('/admin/configuration');
+            });
+        });
+    },
+    editPageDescription: (peticion, respuesta)=>{
+        Configuration.findById('5d8561039709f1199c921feb').then(editDescription=>{
+            editDescription.pageDescription = peticion.body.pageDescription;
+
+            editDescription.save().then(updatedDescription=>{
+                peticion.flash('success-message', `Descripción ha sido actualizada satisfactoriamente`);
+                respuesta.redirect('/admin/configuration');
+            });
+        });
+    },
+    editPageKeyWords: (peticion, respuesta)=>{
+        Configuration.findById('5d8561039709f1199c921feb').then(editKeyWords=>{
+            editKeyWords.pageKeyWords = peticion.body.pageKeyWords;
+
+            editKeyWords.save().then(updatedKeyWords=>{
+                peticion.flash('success-message', `Las palabras clave han sido actualizadas satisfactoriamente`);
+                respuesta.redirect('/admin/configuration');
+            });
+        });
+    },
+    /*Todos los metodos para las plantillas*/
+    getTemplates: (peticion, respuesta)=>{ 
+        Template.find().then(templates =>{
+            respuesta.render('admin/templates/index', {templates: templates});
+        });
+    },
+    createTemplate: (peticion, respuesta)=>{ 
+        respuesta.render('admin/templates/create'); 
+    },
+    submitTemplates: (peticion, respuesta)=>{
+        //Validación del Input File
+        let fileName1 = peticion.body.title+'.html';
+        let fileName2 = 'styles.css';
+        let fileName3 = 'script.js';
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.uploadedFile1;
+            //fileName1 = file.name;
+            let uploadDir = `./public/templates/${peticion.body.title}/`;
+                if (!fs.existsSync(uploadDir)){
+                    fs.mkdirSync(uploadDir);
+                }
+
+            file.mv(uploadDir+fileName1, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.uploadedFile2;
+            //fileName2 = file.name;
+            let uploadDir = `./public/templates/${peticion.body.title}/`;
+                if (!fs.existsSync(uploadDir)){
+                    fs.mkdirSync(uploadDir);
+                }
+                
+            file.mv(uploadDir+fileName2, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        if(!isEmpty(peticion.files)){
+            let file = peticion.files.uploadedFile3;
+            //fileName3 = file.name;
+            let uploadDir = `./public/templates/${peticion.body.title}/`;
+                if (!fs.existsSync(uploadDir)){
+                    fs.mkdirSync(uploadDir);
+                }
+
+            file.mv(uploadDir+fileName3, (error)=>{
+                if(error)
+                throw error;
+            });
+        }
+
+        const newTemplate = new Template({
+            title: peticion.body.title,
+            description: peticion.body.description,
+            htmlFile: `templates/${peticion.body.title}/${fileName1}`,
+            cssFile: `templates/${peticion.body.title}/${fileName2}`,
+            jsFile: `templates/${peticion.body.title}/${fileName3}`
+        });
+
+        newTemplate.save().then(template =>{
+            console.log(template);
+            peticion.flash('success-message', 'Plantilla creada satisfactoriamente.');
+            respuesta.redirect('/admin/template');
+        });
+    },
+    activeTemplate: (peticion, respuesta)=>{
+        const id = peticion.params.id;
+
+        Template.findById(id).then(template=>{
+            template.isActive = true;
+
+            template.save().then(updateTemplate=>{
+                peticion.flash('success-message', `Se ha establecido la plantilla`);
+                respuesta.redirect('/admin/template');
+            });
+        });
+    }
+}
+
+ 
--- controllers/defaultController.js
+++ controllers/defaultController.js
@@ -1,17 +1,115 @@
+var Post = require('../models/postModel').Post;
+var Template = require('../models/TemplateModel').Template;
+var Category = require('../models/CategoryModel').Category;
+var Comment = require('../models/CommentModel').Comment;
+var Configuration = require('../models/ConfigurationModel').Configuration;
+var bcrypt = require('bcryptjs');
+var User = require('../models/UserModel').User;
+
 module.exports = {
-    index: (peticion, respuesta)=>{
-        respuesta.render('default/index');
+    index: async (peticion, respuesta)=>{
+        const posts = await Post.find();
+        const categories = await Category.find();
+        const templates = await Template.find();
+        const configurations = await Configuration.find();
+        respuesta.render('default/index', {posts: posts, categories: categories, templates: templates, configurations: configurations});
     },
+    /*Login Routes*/
     loginGet: (peticion, respuesta)=>{
-        respuesta.render('default/login');
+        respuesta.render('default/login', {message: peticion.flash('error')});
     },
     loginPost: (peticion, respuesta)=>{
-        respuesta.send('hola')
+        
     },
     registerGet: (peticion, respuesta)=>{
         respuesta.render('default/register');
     },
     registerPost: (peticion, respuesta)=>{
-        respuesta.send("como esta");
+        let errors = [];
+
+        if(!peticion.body.firstName){
+            errors.push({message: 'El nombre es necesario'});
+        }
+
+        if(!peticion.body.lastName){
+            errors.push({message: 'El apellido es necesario'});
+        }
+
+        if(!peticion.body.email){
+            errors.push({message: 'El email es necesario'})
+        }
+
+        if(!peticion.body.password || !peticion.body.passwordConfirm){
+            errors.push({message: 'La contraseña es necesaria'});
+        }
+
+        if(peticion.body.password !== peticion.body.passwordConfirm){
+            errors.push({message: 'Las contraseñas no coinciden'})
+        }
+
+        if(errors.length > 0){
+            respuesta.render('default/register', {
+                errors: errors,
+                firstName: peticion.body.firstName,
+                lastName: peticion.body.lastName,
+                email: peticion.body.email
+            });
+        }else{
+            User.findOne({email: peticion.body.email}).then(user=>{
+                if(user){
+                    peticion.flash('error-message', 'Ya existe este correo electrónico, intente iniciar sesión.');
+                    respuesta.redirect('/login');
+                }else{
+                    const newUser = new User(peticion.body);
+
+                    bcrypt.genSalt(10, (err, salt)=>{
+                        bcrypt.hash(newUser.password, salt, (err, hash)=>{
+                            newUser.password = hash;
+                            newUser.save().then(user=>{
+                                peticion.flash('success-message', 'Te has registrado satisfactoriamente');
+                                respuesta.redirect('/login');
+                            });
+                        });
+                    });
+                }
+            })
+        }
+    },
+
+    getSinglePost: async (peticion, respuesta)=>{
+        const id = peticion.params.id;
+        const configurations = await Configuration.find();
+        const categories = await Category.find();
+
+        Post.findById(id).populate({path: 'comments', populate: {path: 'user', model: 'user'}}).then(post=>{
+            if(!post){
+                respuesta.status(404).json({message: 'Post no encontrado'});
+            }else{
+                respuesta.render('default/singlePost', {post: post, comments: post.comments, configurations: configurations, categories: categories});
+            }
+        });
+    },
+
+    submitComment: (peticion, respuesta)=>{
+        if(peticion.user){
+            Post.findById(peticion.body.id).then(post=>{
+                const newComment = new Comment({
+                    user: peticion.user.id,
+                    body: peticion.body.comment_body
+                });
+
+                post.comments.push(newComment);
+                post.save().then(savedPost=>{
+                    newComment.save().then(savedComment=>{
+                        peticion.flash('success-message', 'Su comentario fue enviado para su revisión');
+                        respuesta.redirect(`/post/${post._id}`);
+                    });
+                });
+            });
+        }else{
+            peticion.flash('error-message', 'Por favor, inicie sesión para dejar un comentario');
+            respuesta.redirect('/login');
+        }
     }
+    
 };
